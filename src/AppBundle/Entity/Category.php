604df68db3f56d65ea4346d9b2b42cc09172a801
RemiFrontiere <remi.frontiere@gmail.com>
2018-02-06 11:05:31 +0100

    Add database :  * change parameter  * create database column (Show & Category)

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -2,7 +2,56 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+
 class Category{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
     private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     private $name;
+
+
+
+
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
--- src/AppBundle/Entity/Show.php
+++ src/AppBundle/Entity/Show.php
@@ -2,12 +2,162 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+
+
 class Show{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
     private $id;
+
+    /**
+     * @ORM\Column
+     */
     private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
     private $abstract;
+
+    /**
+     * @ORM\Column
+     */
     private $country;
+
+    /**
+     * @ORM\Column
+     */
     private $author;
+
+    /**
+     * @ORM\Column(type="date")
+     */
     private $releasedDate;
+
+    /**
+     * @ORM\Column
+     */
     private $mainPicture;
+
+    private $category;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAbstract()
+    {
+        return $this->abstract;
+    }
+
+    /**
+     * @param mixed $abstract
+     */
+    public function setAbstract($abstract)
+    {
+        $this->abstract = $abstract;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param mixed $category
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMainPicture()
+    {
+        return $this->mainPicture;
+    }
+
+    /**
+     * @param mixed $mainPicture
+     */
+    public function setMainPicture($mainPicture)
+    {
+        $this->mainPicture = $mainPicture;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getReleasedDate()
+    {
+        return $this->releasedDate;
+    }
+
+    /**
+     * @param mixed $releasedDate
+     */
+    public function setReleasedDate($releasedDate)
+    {
+        $this->releasedDate = $releasedDate;
+    }
 }
