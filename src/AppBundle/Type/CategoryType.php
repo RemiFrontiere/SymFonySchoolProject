edd3ba2e83cad803d706a4b1e350eacc344e1b40
Dev <remi.frontiere@gmail.com>
2018-02-07 21:34:57 +0100

    Create

---
+++ src/AppBundle/Type/CategoryType.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace AppBundle\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('save', SubmitType::class);
+    }
+}
+ ?>
