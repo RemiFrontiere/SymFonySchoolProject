e80a4eea74d70f24f744e77c378069b9c223bd3d
Dev <dev@Dev.home>
2018-02-12 11:20:16 +0100

    user Type & Controller

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Type\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
 * @Route("/user", name="user_")
@@ -16,7 +19,22 @@ class UserController extends Controller{
   */
   public function createAction(){
 
-    return $this->render('user/create.html.twig');
+    $user = new User();
+    $userForm = $this->createForm(UserType::class, $user);
+
+    $userForm->handleRequest($request);
+
+    if($userForm->isValid()){
+      $em = $this->getDoctrine()->getManager();
+
+      $em->persist($user);
+      $em->flush();
+
+      $this->addFlash('success', 'The user has beeen successfully added.');
+      return $this->redirectToRoute('show_list');
+    }
+
+    return $this->render('user/create.html.twig', ['userForm' => $userForm->createForm()]);
   }
 
 
