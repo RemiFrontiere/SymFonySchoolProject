6f35d91bd4264d786f9e03ef9ef72f7b63fecc51
RemiFrontiere <remi.frontiere@gmail.com>
2018-04-17 16:19:26 +0200

    finish

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -1,18 +1,24 @@
 <?php
-
 namespace Tests\AppBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
 class DefaultControllerTest extends WebTestCase
 {
+	private $client;
     public function testIndex()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
-        //$this->assertEquals(200, $client->getResponse()->getStatusCode());
-        //$this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $crawler = $this->client->request('GET', '/');
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $crawler = $this->client->followRedirect();
+        $this->assertContains('Welcome', $crawler->filter('h1')->text());
+        $crawler = $this->client->back();
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+    }
+    public function setUp()
+    {
+    	$this->client = static::createClient();
+    }
+    public function tearDown()
+    {
+    	$this->client = null;
     }
 }
